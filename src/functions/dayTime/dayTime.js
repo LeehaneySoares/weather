c5ca8f16b330e3b2067c20f9e6799ba83121e28e
Leehaney Soares <leehaney.soares@gmail.com>
2022-09-22 09:34:48 -0300

    chore(weather.functions): criando função que altera o background de acordo com o horario

--- src/functions/dayTime/dayTime.js
+++ src/functions/dayTime/dayTime.js
@@ -1,9 +1,26 @@
-import schema from './schema.json'
+import schema from './schema'
+
+const date = new Date()
+const hours = new Intl.DateTimeFormat('pt-BR', {
+  hour: 'numeric',
+  minute: 'numeric',
+  timeZoneName: 'short'
+}).format(date)
 
 export default function () {
-  const date = new Date()
-  console.log(new Date().getTimezoneOffset())
   const body = document.querySelector('body')
 
-  body.style['background-image'] = `url('${schema.morning}')`
+  hours >= schema.morning.hours &&
+  hours < schema.afternoon.hours && (
+    body.style['background-image'] = `url('${schema.morning.image}')`
+  )
+
+  hours >= schema.afternoon.hours &&
+  hours < schema.night.hours && (
+    body.style['background-image'] = `url('${schema.afternoon.image}')`
+  )
+
+  hours >= schema.night.hours && (
+    body.style['background-image'] = `url('${schema.night.image}')`
+  )
 }
